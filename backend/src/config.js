66fab2c9b99e60881ee04a461c2c981d24eebafb
¨ERICKSOMMET¨ <ericksommet@yahoo.com>
2022-04-11 20:17:07 -0500

    change date

--- backend/src/config.js
+++ backend/src/config.js
@@ -87,7 +87,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-13T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-14T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xF2D6980Ee3D35ce3563Cf4c97848230f013D52B3"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
